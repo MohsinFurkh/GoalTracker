7b098ed4f0a9ac1dd50661b1cb6ab53c92be2d10
mohsinfaurkh <mohsinfaurkh@gmail.com>
2025-05-08 10:35:52 +0000

    fix: Remove SessionProvider from _app.js to fix CLIENT_FETCH_ERROR

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import { SessionProvider } from 'next-auth/react';
 import { NotificationProvider } from '../components/common/NotificationSystem';
 import theme from '../styles/theme';
 import Head from 'next/head';
@@ -8,28 +7,26 @@ import Head from 'next/head';
  * Custom App component that wraps the entire application
  * Provides global contexts and theme
  */
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <SessionProvider session={session}>
-      <Head>
-        <title>GoalTrackr - Track Your Goals</title>
-        <meta name="description" content="Track and achieve your goals with GoalTrackr" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/Image.jpeg" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#1976d2" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-        />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <NotificationProvider>
-          <Component {...pageProps} />
-        </NotificationProvider>
-      </ThemeProvider>
-    </SessionProvider>
+    <Head>
+      <title>GoalTrackr - Track Your Goals</title>
+      <meta name="description" content="Track and achieve your goals with GoalTrackr" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/Image.jpeg" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#1976d2" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+      />
+    </Head>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NotificationProvider>
+        <Component {...pageProps} />
+      </NotificationProvider>
+    </ThemeProvider>
   );
 }
 
